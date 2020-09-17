5fea02da7098f09484570433ccb24cd7bf9b3ed9
RUIFANXU <ruifan831@gmail.com>
2020-09-16 20:45:43 -0700

    first commit

---
+++ utils.py
@@ -0,0 +1,188 @@
+import numpy as np
+from knn import KNN
+
+############################################################################
+# DO NOT MODIFY CODES ABOVE 
+############################################################################
+
+
+# TODO: implement F1 score
+def f1_score(real_labels, predicted_labels):
+    """
+    Information on F1 score - https://en.wikipedia.org/wiki/F1_score
+    :param real_labels: List[int]
+    :param predicted_labels: List[int]
+    :return: float
+    """
+    assert len(real_labels) == len(predicted_labels)
+
+    recall = np.dot(real_labels,predicted_labels)/ np.count_nonzero(np.array(real_labels)==1)
+    precision= np.dot(real_labels,predicted_labels)/ np.count_nonzero(np.array(predicted_labels)==1)
+    return 2* precision*recall/(precision+recall)
+
+
+class Distances:
+    @staticmethod
+    # TODO
+    def minkowski_distance(point1, point2):
+        """
+        Minkowski distance is the generalized version of Euclidean Distance
+        It is also know as L-p norm (where p>=1) that you have studied in class
+        For our assignment we need to take p=3
+        Information on Minkowski distance - https://en.wikipedia.org/wiki/Minkowski_distance
+        :param point1: List[float]
+        :param point2: List[float]
+        :return: float
+        """
+        return np.power(np.sum(np.power(np.subtract(point1,point2),3)),1/3)
+
+    @staticmethod
+    # TODO
+    def euclidean_distance(point1, point2):
+        """
+        :param point1: List[float]
+        :param point2: List[float]
+        :return: float
+        """
+        return np.sqrt(np.sum(np.power(np.subtract(point1,point2),2)))
+
+
+    @staticmethod
+    # TODO
+    def cosine_similarity_distance(point1, point2):
+        """
+       :param point1: List[float]
+       :param point2: List[float]
+       :return: float
+       """
+        norm1 = np.linalg.norm(point1)
+        norm2 = np.linalg.norm(point2)
+        numerator = np.dot(point1,point2)
+        return 1 - numerator/(norm1*norm2)
+
+
+
+class HyperparameterTuner:
+    def __init__(self):
+        self.best_k = None
+        self.best_distance_function = None
+        self.best_scaler = None
+        self.best_model = None
+
+    # TODO: find parameters with the best f1 score on validation dataset
+    def tuning_without_scaling(self, distance_funcs, x_train, y_train, x_val, y_val):
+        """
+        In this part, you need to try different distance functions you implemented in part 1.1 and different values of k (among 1, 3, 5, ... , 29), and find the best model with the highest f1-score on the given validation set.
+		
+        :param distance_funcs: dictionary of distance functions (key is the function name, value is the function) you need to try to calculate the distance. Make sure you loop over all distance functions for each k value.
+        :param x_train: List[List[int]] training data set to train your KNN model
+        :param y_train: List[int] training labels to train your KNN model
+        :param x_val:  List[List[int]] validation data
+        :param y_val: List[int] validation labels
+
+        Find the best k, distance_function (its name), and model (an instance of KNN) and assign them to self.best_k,
+        self.best_distance_function, and self.best_model respectively.
+        NOTE: self.best_scaler will be None.
+
+        NOTE: When there is a tie, choose the model based on the following priorities:
+        First check the distance function:  euclidean > Minkowski > cosine_dist 
+		(this will also be the insertion order in "distance_funcs", to make things easier).
+        For the same distance function, further break tie by prioritizing a smaller k.
+        """
+        
+        # You need to assign the final values to these variables
+        self.best_k = None
+        self.best_distance_function = None
+        self.best_model = None
+        best_list=[]
+        for k,v in distance_funcs.items():
+            print(k)
+            f1_scores =[self.helper(i,v,x_train,y_train,x_val,y_val) for i in range(1,30)]
+            
+            temp_k = np.argmax(f1_scores)
+            temp_score = f1_scores[temp_k]
+            best_list.append((temp_score,temp_k,k))
+        temp = sorted(best_list,reverse=True)[0]
+        self.best_k = temp[1]
+        self.best_distance_function=temp[2]
+        self.best_model = KNN(self.best_k,distance_funcs[self.best_distance_function])
+        self.best_scaler = None
+    
+    def helper(self,k,dis_function,x_train,y_train,x_val,y_val):
+        knn = KNN(k,dis_function)
+        knn.train(x_train,y_train)
+        print(f1_score(y_val,knn.predict(x_val)))
+        return f1_score(y_val,knn.predict(x_val))
+        
+
+        
+         
+                
+
+    # TODO: find parameters with the best f1 score on validation dataset, with normalized data
+    def tuning_with_scaling(self, distance_funcs, scaling_classes, x_train, y_train, x_val, y_val):
+        """
+        This part is the same as "tuning_without_scaling", except that you also need to try two different scalers implemented in Part 1.3. More specifically, before passing the training and validation data to KNN model, apply the scalers in scaling_classes to both of them. 
+		
+        :param distance_funcs: dictionary of distance functions (key is the function name, value is the function) you need to try to calculate the distance. Make sure you loop over all distance functions for each k value.
+        :param scaling_classes: dictionary of scalers (key is the scaler name, value is the scaler class) you need to try to normalize your data
+        :param x_train: List[List[int]] training data set to train your KNN model
+        :param y_train: List[int] train labels to train your KNN model
+        :param x_val: List[List[int]] validation data
+        :param y_val: List[int] validation labels
+
+        Find the best k, distance_function (its name), scaler (its name), and model (an instance of KNN), and assign them to self.best_k, self.best_distance_function, best_scaler, and self.best_model respectively.
+        
+        NOTE: When there is a tie, choose the model based on the following priorities:
+        First check scaler, prioritizing "min_max_scale" over "normalize" (which will also be the insertion order of scaling_classes). Then follow the same rule as in "tuning_without_scaling".
+        """
+        
+        # You need to assign the final values to these variables
+        self.best_k = None
+        self.best_distance_function = None
+        self.best_scaler = None
+        self.best_model = None
+        raise NotImplementedError
+
+
+class NormalizationScaler:
+    def __init__(self):
+        pass
+
+    # TODO: normalize data
+    def __call__(self, features):
+        """
+        Normalize features for every sample
+
+        Example
+        features = [[3, 4], [1, -1], [0, 0]]
+        return [[0.6, 0.8], [0.707107, -0.707107], [0, 0]]
+
+        :param features: List[List[float]]
+        :return: List[List[float]]
+        """
+        norms = np.linalg.norm(features,axis=1).reshape(-1,1)
+        return features/norms
+
+
+class MinMaxScaler:
+    def __init__(self):
+        pass
+
+    # TODO: min-max normalize data
+    def __call__(self, features):
+        """
+		For each feature, normalize it linearly so that its value is between 0 and 1 across all samples.
+        For example, if the input features are [[2, -1], [-1, 5], [0, 0]],
+        the output should be [[1, 0], [0, 1], [0.333333, 0.16667]].
+		This is because: take the first feature for example, which has values 2, -1, and 0 across the three samples.
+		The minimum value of this feature is thus min=-1, while the maximum value is max=2.
+		So the new feature value for each sample can be computed by: new_value = (old_value - min)/(max-min),
+		leading to 1, 0, and 0.333333.
+		If max happens to be same as min, set all new values to be zero for this feature.
+		(For further reference, see https://en.wikipedia.org/wiki/Feature_scaling.)
+
+        :param features: List[List[float]]
+        :return: List[List[float]]
+        """
+        return np.subtract(features,np.min(features,axis=0))/(np.max(features,axis=0)-np.min(features,axis=0))
